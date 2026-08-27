b892a2ebf77fabe637e82bb36224dfa4fe9866ff
Jeffrey Vincent <jeffreyj.vincent@yahoo.com>
2026-08-27 14:51:52 -0400

    cleanup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import { AuthProvider } from "./auth";
 import "./styles.css";
@@ -21,7 +20,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
-      <Analytics />
     </AuthProvider>
   </React.StrictMode>
 );
